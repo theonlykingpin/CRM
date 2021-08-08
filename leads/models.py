f914857915a031b0a3bab931becd54c13c380c3c
theonlykingpin <theonlykingpin@outlook.com>
2021-08-08 15:41:05 +0430

    models created

--- leads/models.py
+++ leads/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
 
+class Agent(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name  = models.CharField(max_length=20)
+
+
 class Lead(models.Model):
     first_name = models.CharField(max_length=20)
     last_name  = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
+    agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
