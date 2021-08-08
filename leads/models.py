82a371685a382c80761a09efdbad35fc1e97d839
theonlykingpin <theonlykingpin@outlook.com>
2021-08-08 17:26:37 +0430

    first view

--- leads/models.py
+++ leads/models.py
@@ -9,9 +9,15 @@ class User(AbstractUser):
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.user.username
+
 
 class Lead(models.Model):
     agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=20)
     last_name  = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
