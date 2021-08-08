9404fd14fcec9ff172de167e627169a3a11dc6ad
theonlykingpin <theonlykingpin@outlook.com>
2021-08-08 16:18:31 +0430

    models changed

--- leads/models.py
+++ leads/models.py
@@ -1,13 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    pass
 
 
 class Agent(models.Model):
-    first_name = models.CharField(max_length=20)
-    last_name  = models.CharField(max_length=20)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 
 class Lead(models.Model):
+    agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=20)
     last_name  = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
-    agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
