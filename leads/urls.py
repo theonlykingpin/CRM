c606249b3bb9fc6a31a52362f8d5e14827692669
theonlykingpin <theonlykingpin@outlook.com>
2021-08-09 16:20:18 +0430

    templates fixed

--- leads/urls.py
+++ leads/urls.py
@@ -4,7 +4,9 @@ from . import views
 app_name = "leads"
 
 urlpatterns = [
-    path('', views.lead_list, name='lead_list'),
-    path('<int:pk>/', views.lead_detail, name='lead_detail'),
-    path('create/', views.lead_create, name='lead_create'),
+    path('', views.lead_list, name='lead-list'),
+    path('<int:pk>/', views.lead_detail, name='lead-detail'),
+    path('create/', views.lead_create, name='lead-create'),
+    path('<int:pk>/update/', views.lead_update, name='lead-update'),
+    path('<int:pk>/delete/', views.lead_delete, name='lead-delete'),
 ]
--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Lead, Agent
-from .forms import LeadForm
+from .forms import LeadModelForm
 
 
 def lead_list(request):
@@ -18,14 +18,29 @@ def lead_detail(request, pk):
 
 
 def lead_create(request):
-    form = LeadForm()
+    form = LeadModelForm()
     if request.method == "POST":
-        form = LeadForm(request.POST)
+        form = LeadModelForm(request.POST)
         if form.is_valid():
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
-            age = form.cleaned_data['age']
-            agent = Agent.objects.first()
-            Lead.objects.create(first_name=first_name, last_name=last_name, age=age, agent=agent)
+            form.save()
+            return redirect('/leads/')
     context = {'form': form}
     return render(request, 'leads/lead_create.html', context)
+
+
+def lead_update(request, pk):
+    lead = Lead.objects.get(id=pk)
+    form = LeadModelForm(instance=lead)
+    if request.method == "POST":
+        form = LeadModelForm(data=request.POST, instance=lead)  # instance comes from database so data grabbed from database and put to the form and data from "POST" request replaces it.
+        if form.is_valid():
+            form.save()
+            return redirect('/leads/' + str(pk) + '/')
+    context = {"lead": lead, "form": form}
+    return render(request, 'leads/lead_update.html', context)
+
+
+def lead_delete(request, pk):
+    lead = Lead.objects.get(id=pk)
+    lead.delete()
+    return redirect('/leads/')
