d0e8086e0c7a9d565898f7f8baddc28dcdb10bec
theonlykingpin <theonlykingpin@outlook.com>
2021-08-09 14:46:06 +0430

    lead create added and some changes to lead list and lead detail

--- leads/urls.py
+++ leads/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = "leads"
 
 urlpatterns = [
-    path('', views.home_page, name='home_page')
+    path('', views.lead_list, name='lead_list'),
+    path('<int:pk>/', views.lead_detail, name='lead_detail'),
+    path('create/', views.lead_create, name='lead_create'),
 ]
--- leads/views.py
+++ leads/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
+from .models import Lead, Agent
+from .forms import LeadForm
 
 
-def home_page(request):
-    return render(request, 'home_page.html', {})
+def lead_list(request):
+    leads = Lead.objects.all()
+    context = {
+        "leads": leads
+    }
+    return render(request, 'leads/lead_list.html', context)
+
+
+def lead_detail(request, pk):
+     lead = Lead.objects.get(id=pk)
+     context = {"lead": lead}
+     return render(request, 'leads/lead_detail.html', context)
+
+
+def lead_create(request):
+    form = LeadForm()
+    if request.method == "POST":
+        form = LeadForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            age = form.cleaned_data['age']
+            agent = Agent.objects.first()
+            Lead.objects.create(first_name=first_name, last_name=last_name, age=age, agent=agent)
+    context = {'form': form}
+    return render(request, 'leads/lead_create.html', context)
